50d37e6d31fc80b2d4344c4c145a22964038fd52
Fahmi Arzalega <fahmiarza@gmail.com>
2026-04-05 00:12:48 +0700

    feat: update list

--- controllers/list_controller.go
+++ controllers/list_controller.go
@@ -5,6 +5,7 @@ import (
 	"github.com/fahmiarz/project-management/services"
 	"github.com/fahmiarz/project-management/utils"
 	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
 )
 
 type ListController struct {
@@ -24,4 +25,42 @@ func (c *ListController) CreateList(ctx *fiber.Ctx) error{
 		return utils.BadRequest(ctx, "Gagal membuat list", err.Error())
 	}
 	return utils.Success(ctx, "List berhasil dibuat", list)
+}
+
+func (c *ListController) UpdateList (ctx *fiber.Ctx) error {
+	publicID := ctx.Params("id")
+	list := new (models.List)
+
+	//gabungkan apa yang ada di body parameter ke dalam variable list
+	if err := ctx.BodyParser(list); err != nil {
+		return utils.BadRequest(ctx, "Gagal Parsing Data", err.Error())
+	}
+
+	//validasi publid id
+	if _,err := uuid.Parse(publicID) ; err != nil {
+		return utils.BadRequest(ctx, "ID tidak valid", err. Error())
+	}
+
+	//verifikasi list yang di update ada atau tidak
+	existingList , err := c.service.GetByPublicID(publicID)
+	if err != nil {
+		return utils.NotFound(ctx, "List tidak ditermukan", err.Error())
+	}
+
+	//gabungkan apa yang ada di variable existingList ke dalam variable list
+	list.InternalID = existingList.InternalID
+	list.PublicID = existingList.PublicID
+
+	if err := c.service.Update(list); err !=nil {
+		return utils.BadRequest(ctx, "Gagal update list", err.Error())
+	}
+
+	//mengambil data yang sudah di update
+	updatedList , err := c.service.GetByPublicID(publicID)
+	if err != nil {
+		return utils.NotFound(ctx, "List tidak ditemukan", err.Error())
+	}
+
+	return utils.Success(ctx , "Berhasil memperbarui list" , updatedList)
+
 }
